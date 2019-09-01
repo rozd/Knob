b657b23bb11ff18fd0cba170153ab83730aa779f
Max Rozdobudko <max.rozdobudko@gmail.com>
2019-09-01 10:07:04 +0300

    Minor changes: refactors internal structure for default values.

--- Knob/Sources/Knob/Knob+Defaults.swift
+++ Knob/Sources/Knob/Knob+Defaults.swift
@@ -11,9 +11,10 @@ import UIKit
 
 extension Knob {
 
-    struct Defaults {
-        static let defaultTrackColor: UIColor = UIColor(red: 226 / 255, green: 226 / 255, blue: 230 / 255, alpha: 1.0)
-        static let defaultFillColor: UIColor  = .systemBlue
+    struct Default {
+        static let trackColor: UIColor = UIColor(red: 226 / 255, green: 226 / 255, blue: 230 / 255, alpha: 1.0)
+        static let fillColor: UIColor  = .systemBlue
+        static let markerSize: CGFloat = 8.0
     }
 
 }
